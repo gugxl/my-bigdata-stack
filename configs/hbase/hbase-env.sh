9e22b80a738a9c6a8974789d313e4bb08b0b68f1
gugu <1104534797@qq.com>
2026-02-12 01:03:06 +0800

    添加内存配置

--- configs/hbase/hbase-env.sh
+++ configs/hbase/hbase-env.sh
@@ -1,2 +1,7 @@
 # my-bigdata-stack/configs/hbase/hbase-env.sh
 export HBASE_MANAGES_ZK=false
+
+# JVM heap size settings
+export HBASE_HEAPSIZE=4096
+export HBASE_REGIONSERVER_OPTS="-Xmx4g -Xms4g"
+export HBASE_MASTER_OPTS="-Xmx2g -Xms2g"
